d3849ef64d634a0a0fc4d48d753936edbf02883b
julianainformatorio <juliana.marighetti18@gmail.com>
2025-05-07 15:10:19 -0300

    clase 2 - 20240430

---
+++ 20250430/estructuras-control.py
@@ -0,0 +1,31 @@
+# #Condicional simple
+# if condicion:
+#     print('Se cumple la condicion') 
+
+# #Condicional alternativo
+# if condicion:
+#     print('Se cumple la condicion')
+# else:
+#     print('No se cumple condicion')
+
+# #Condicional multiple
+# if condicion:
+#     print('Se cumple la condicion')
+# elif(condicion_2):
+#     print('Se cumple la condición')
+# elif(condicion_3):
+#     print('Se cumple la funcion')
+
+#iterativos
+# variables = [11,22,33,44,55]
+# for x in variables:
+#     print('Estoy en el elemento', x)
+
+# for elemento in range(100):
+#     print('Estoy en el elemento', elemento)
+
+#Cortamos el proceso con ctrl+c
+contador = 0
+while (contador == 5):
+    print('Contador vale:', contador)
+    contador = contador + 1
---
+++ 20250430/variables-tipo-dato.py
@@ -0,0 +1,54 @@
+# ##Variables enteras
+# int_1 = 5
+# int_2 = 2
+# ##Operadores
+# #suma
+# suma = int_1 + int_2
+# print(f"La suma entre {int_1} y {int_2} es: {suma}")
+
+# #resta
+# resta = int_1 - int_2
+# print(f"La resta entre {int_1} y {int_2} es: {resta}")
+# #multiplicación 
+# multiplicación = int_1 * int_2
+# print(f"La multiplicación entre {int_1} y {int_2} es: {multiplicación}")
+# #división 
+# división = int_1 / int_2
+# print(f"La división entre {int_1} y {int_2} es: {división}")
+# #potencia
+# potencia = int_1 ** int_2
+# print(f"La potencia entre {int_1} y {int_2} es: {potencia}")
+
+##Variables string
+# str_1 = "Info"
+# str_2 = "rmatorio"
+##Operadores
+# suma = str_1 + str_2
+# print(f"La suma entre {str_1} y {str_2} es: {suma}")
+##Variables listas 
+# lis_1 = [1,2,3]
+# lis_2 = [4,5,6]
+# #Operadores 
+# #suma 
+# suma = lis_1 + lis_2
+# print(f"La suma entre {lis_1} y {lis_2} es: {suma}")
+
+# lista = [93,1238,203,21982,38747.23,123,23,1,1,23,4]
+# # utilizando .sort ()
+# lista.sort()
+# print(lista)
+# print(lista)
+# 0, 1, 2, 3, 4 ........
+# lista = [10,20,30,40,50]
+# print(lista[-1])
+
+# lista = ['Gerardo', 'Andrés', 'Ainara']
+# lista.sort ()
+# print (lista)
+
+# tupla = ('Gerardo', 'Andrés', 'Ainara')
+# print.tupla()
+
+# diccionario = {'40031967': 'Juliana Marighetti', '43568897': 'Pablo Perez'}
+# print(diccionario[40031967])
+
